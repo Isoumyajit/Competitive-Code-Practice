79160af2c0806d5627379fd3da3d8091fe656858
Soumyajit Chakraborty <soumyo.chak1999@gmail.com>
2020-12-24 21:00:55 +0530

    odd length subarray sum

---
+++ leetcode/Sum of All Odd Length Subarrays.py
@@ -0,0 +1,81 @@
+# Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
+#
+# A subarray is a contiguous subsequence of the array.
+#
+# Return the sum of all odd-length subarrays of arr.
+# Example
+# 1:
+#
+# Input: arr = [1, 4, 2, 5, 3]
+# Output: 58
+# Explanation: The
+# odd - length
+# subarrays
+# of
+# arr and their
+# sums
+# are:
+# [1] = 1
+# [4] = 4
+# [2] = 2
+# [5] = 5
+# [3] = 3
+# [1, 4, 2] = 7
+# [4, 2, 5] = 11
+# [2, 5, 3] = 10
+# [1, 4, 2, 5, 3] = 15
+# If
+# we
+# add
+# all
+# these
+# together
+# we
+# get
+# 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+# Example
+# 2:
+#
+# Input: arr = [1, 2]
+# Output: 3
+# Explanation: There
+# are
+# only
+# 2
+# subarrays
+# of
+# odd
+# length, [1] and [2].Their
+# sum is 3.
+# Example
+# 3:
+#
+# Input: arr = [10, 11, 12]
+# Output: 66
+#
+# Constraints:
+#
+# 1 <= arr.length <= 100
+#  1 <= arr[i] <= 1000
+
+n = int(input())
+lst = []
+for i in range(n):
+    lst.append(int(input()))
+temp = []
+j = 1
+sum_l = 0
+for i in range(len(lst)):
+    for k in range(len(lst)):
+        temp.append(lst[k:k+j])
+        sum_l += sum(lst[k:k+j])
+        if k+j == len(lst):
+            break
+    if j == len(lst):
+        break
+    j += 2
+    if j > len(lst):
+        break
+
+print(sum_l)
+print(temp)
