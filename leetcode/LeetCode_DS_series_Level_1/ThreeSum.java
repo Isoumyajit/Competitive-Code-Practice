e2d49f5a7763c3c5480def3bac0efd0f8c177f74
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2022-04-15 12:27:38 +0530

    Leetcode DS Practice league day 2

--- leetcode/LeetCode_DS_series_Level_1/ThreeSum.java
+++ leetcode/LeetCode_DS_series_Level_1/ThreeSum.java
@@ -1,3 +1,25 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Example 2:
+
+// Input: nums = []
+// Output: []
+// Example 3:
+
+// Input: nums = [0]
+// Output: []
+
+// Constraints:
+
+// 0 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,31 +43,57 @@ public class ThreeSum {
 class SolutionThreeSum {
     public static List<List<Integer>> threeSum(int[] nums) {
 
-        int len = nums.length;
-        Arrays.sort(nums);
-
         List<List<Integer>> answer = new ArrayList<List<Integer>>();
-
-        ArrayList<Integer> counter = new ArrayList<Integer>();
-        for (int val : nums) {
-            counter.add(val);
-        }
-        for (int i = 0, j = len - 1; i + 2 <= j;) {
-            int sum = nums[i] + nums[j];
-            if (counter.contains(sum * -1)) {
-                if (counter.indexOf(sum * -1) != j) {
-                    List<Integer> dummy = new ArrayList<Integer>();
-                    dummy.add(nums[i]);
-                    dummy.add(sum * -1);
-                    dummy.add(nums[j]);
-                    j--;
-                    answer.add(dummy);
-                    // dummy.clear();
+        int len = nums.length;
+        int sum = 0;
+        if (len < 3)
+            return answer;
+        if (len == 3) {
+            for (int i = 0; i < len; i++) {
+                sum += nums[i];
+            }
+            if (sum == 0) {
+                List<Integer> dummy_ = new ArrayList<Integer>();
+                for (int i : nums) {
+                    dummy_.add(i);
                 }
-            } else {
-                i++;
+
+                answer.add(dummy_);
+                return answer;
             }
         }
+        Arrays.sort(nums);
+
+        for (int i = 0; i < len - 2; i++) {
+
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int low = i + 1;
+                int high = len - 1;
+                sum = -1 * nums[i];
+
+                // Two pointer Algorithm starts from here
+
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        answer.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+                        while (low < high && nums[low] == nums[low + 1]) {
+                            low++;
+                        }
+                        while (low < high && nums[high] == nums[high - 1]) {
+                            high--;
+                        }
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] > sum)
+                        high--;
+                    else
+                        low++;
+
+                }
+            }
+        }
+
         return answer;
     }
 }
