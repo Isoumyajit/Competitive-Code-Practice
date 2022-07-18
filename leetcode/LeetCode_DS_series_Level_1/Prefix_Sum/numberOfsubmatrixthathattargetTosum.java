f84b6dfb361e669ae3e92362eb67f01b5af08fc3
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2022-07-19 00:33:28 +0530

    Leetcode daily challenge Submatrixes to target Sum

---
+++ leetcode/LeetCode_DS_series_Level_1/Prefix_Sum/numberOfsubmatrixthathattargetTosum.java
@@ -0,0 +1,71 @@
+package Prefix_Sum;
+
+/*
+ * Given a matrix and a target, return the number of non-empty submatrices that sum to target.
+
+A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
+
+Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
+Output: 4
+Explanation: The four 1x1 submatrices that only contain 0.
+Example 2:
+
+Input: matrix = [[1,-1],[-1,1]], target = 0
+Output: 5
+Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
+Example 3:
+
+Input: matrix = [[904]], target = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= matrix.length <= 100
+1 <= matrix[0].length <= 100
+-1000 <= matrix[i] <= 1000
+-10^8 <= target <= 10^8
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class numberOfsubmatrixthathattargetTosum {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        if (r == 0 && c == 0)
+            return 0;
+        int count = 0;
+
+        for (int index = 0; index < r; index++) {
+            for (int j = 1; j < c; j++) {
+                matrix[index][j] += matrix[index][j - 1];
+            }
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int startcol = 0; startcol < c; startcol++) {
+            for (int curcol = startcol; curcol < c; curcol++) {
+
+                map.clear();
+                map.put(0, 1);
+                int sum = 0;
+                for (int row = 0; row < r; row++) {
+                    sum += matrix[row][curcol] - ((startcol > 0) ? matrix[row][startcol - 1] : 0);
+                    count += map.getOrDefault(sum - target, 0);
+                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+                }
+            }
+        }
+        return count;
+    }
+}
